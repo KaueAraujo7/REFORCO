f0265ab829bed3e6fd9167d38e83f45bc0e26a6f
KaueAraujo7 <kaue.rodrigues29@aluno.ce.gov.br>
2025-12-04 12:44:04 -0300

    two interacao

--- EX1.py
+++ EX1.py
@@ -1,3 +1,7 @@
-for i in range (2):
-    nome =input('digite seu nome:')
-    idade=int(input('digite sua idade:'))
+nome_mais_novo = ""
+idade_mais_novo = 0
+for i in range(10):
+  nome = input('Digite seu nome: ')
+  idade = int(input('Digite sua idade: '))
+print(nome_mais_novo)  
+print(idade_mais_novo) 
